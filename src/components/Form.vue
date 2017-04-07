79150f6011eea8d8e5cbf529a990fa9c8eb3dd16
creabine <creabine@foxmail.com>
2017-04-07 16:59:48 +0800

    调整了form表单元素的排版

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,16 +1,13 @@
 <template>
 	<div class="jumbotron">
 		<h1>表单</h1>
-		<!-- <p>{{status == 1 ? "得到数据" : "无数据"}}</p> -->
 
 		<!-- Form -->
 		<el-button type="primary" @click="getList()">获取并编辑数据</el-button>
 		<el-dialog title="数据编辑表单" v-model="dialogFormVisible"  size="large">
-			<el-form :model="formData" :rules="rules" ref="formData" :inline="true" label-position="top" label-width="120px">
-				<!-- 循环表单目前的问题 -->
-				<!-- 1.如何提交修改vuex中的数据 -->
-				<el-row>
-					<el-col :span="8" :offset="4" v-for="(item,index) in formModel" :key="item.code">
+			<el-row>
+				<el-form :model="formData" :rules="rules" ref="formData" label-position="top" label-width="120px">
+					<el-col :span="9" :offset="2" v-for="(item,index) in formModel" :key="item.code">
 
 						<el-form-item v-if="item.type == 'stat'" :label="item.name" :prop="item.code">
 							<el-radio-group v-model="formData[item.code]">
@@ -20,33 +17,32 @@
 
 						<el-form-item v-else :label="item.name" :prop="item.code">
 							<el-input v-model.number="formData[item.code]" :disabled="item.editable"></el-input>
-						</el-form-item>			
-					</el-col>
-				</el-row>
-				<!-- 提交按钮 -->
-				<el-row type="flex" justify="center">
-						<el-form-item>
-							<el-button @click="dialogFormVisible = false">取 消</el-button>
-							<el-button type="primary" @click="submitForm('formData')">提 交</el-button>
 						</el-form-item>
-				</el-row>
 
-			</el-form>
-			<el-row type="flex" justify="center">
-				<template v-for="list in slaves">
-					<el-button type="primary" @click="renderSlaves(list.code)">{{list.name}}</el-button>
-				</template>
+					</el-col>
+				</el-form>
 			</el-row>
 
-			<el-table v-show="displaySlavesModel.length != 0" :data="displaySlavesData" border stripe style="width:100%;">
+			<!-- 子表渲染 -->
+			<template v-for="list in slaves">
+				<el-row type="flex" justify="center">
+					<p>{{list.name}}</p>
+				</el-row>
+				<el-row type="flex" justify="center">
+					<el-table :data="formData[list.code]" border stripe style="width:100%;">
+							<el-table-column type="selection" width="50"></el-table-column>
+							<template v-for="item in list.fields">
+								<el-table-column :prop="item.code" :label="item.name" width="100"></el-table-column>
+							</template>
+					</el-table>
+				</el-row>
+			</template>
 
-				<el-table-column type="selection" width="50"></el-table-column>
-
-				<template v-for="item in displaySlavesModel">
-					<el-table-column :prop="item.code" :label="item.name" width="100"></el-table-column>
-				</template>
-
-			</el-table>
+			<!-- 提交按钮 -->
+			<el-row type="flex" justify="center" class="btnFixed">
+					<el-button @click="dialogFormVisible = false">取 消</el-button>
+					<el-button type="primary" @click="submitForm('formData')">提 交</el-button>
+			</el-row>
 
 		</el-dialog>
 		
@@ -61,8 +57,6 @@ export default {
 	data(){
 		return {
 			dialogFormVisible: false,
-			displaySlavesModel: [],
-			displaySlavesData: []
 		}
 	},
 	computed : {
@@ -75,7 +69,6 @@ export default {
 		getList() {
 			/* 从json文件取得数据放入vuex  */
 			//pass
-
 			this.dialogFormVisible = true;
 		},
 		submitForm(formName) {
@@ -90,18 +83,6 @@ export default {
 					return false;
 				}
 			});
-		},
-		renderSlaves(code) {
-			/* 根据code取相应的model */
-	    		for (var i = this.slaves.length - 1; i >= 0; i--) {
-	    			if (this.slaves[i].code == code) {
-	    				this.displaySlavesModel = this.slaves[i].fields;
-	    				break;
-	    			}
-	    		}
-	    		/* 根据code取相应的data,没data */
-	    		console.log(this.formData[code]);
-	    		this.displaySlavesData = this.formData[code];
 		}
 	},
 }
